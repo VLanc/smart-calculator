f8e5eed8859131df1fb0045f1154192d79570a41
VLanc <vlancv@gmail.com>
2018-03-10 16:07:34 +0300

    Task completed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,57 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.commonStr = "" + initialValue + "";
   }
 
   add(number) {
-    // your implementation
+    this.commonStr += "+" + number + "";
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.commonStr += "-" + number + "";
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.commonStr += "*" + number + "";
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.commonStr += "/" + number + "";
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    let startPosition, 
+    finishPosition,
+    commonStrToArr = this.commonStr.split("");
+
+    outer: for (let i = commonStrToArr.length - 1; i < commonStrToArr.length; i--) {
+      if (!isNaN(commonStrToArr[i])) {
+        finishPosition = i;
+        for (let j = i - 1; j < commonStrToArr.length; j--) {
+          if (!isNaN(commonStrToArr[j])) {
+            continue;
+          } else {
+            startPosition = j + 1;
+            break outer;
+          }
+        }
+      }
+    }
+
+    let lastNumber = commonStrToArr.slice(startPosition, finishPosition + 1).join("");
+
+    commonStrToArr.splice(startPosition, finishPosition - startPosition + 1, "Math.pow(" + lastNumber + "," + number + ")");
+
+    this.commonStr = commonStrToArr.join("");
+    return this;
+  }
+
+  valueOf() {
+    return eval(this.commonStr);
   }
 }
 
